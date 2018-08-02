48d96120a8845975ca5741eb3c7ab69e45ecd79c
bryan123m <bryanmzr183@gmail.com>
2018-08-01 19:25:11 -0500

    hacer resultado

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -5,13 +5,14 @@ jQuery(document).ready(function($){
 			e.preventDefault();
 
 			var lugar = $('.formulario #lugar').val();
+			console.log(lugar)
 			var cuantos = $('.formulario #cuantos').val();
 			var tipo = $('.formulario #tipo').val();
-			if(!lugar) {
+			if (lugar == 'Selecciona el lugar') {
 				alert('Debe ingresar un lugar');
-			}else if(!cuantos) {
+			}else if (lugar == 'Selecciona el lugar') {
 				alert('Debe ingresar cuantos son');
-			}else if (!tipo) {
+			}else if (lugar == 'Selecciona el lugar') {
 				alert('Debe ingresar un tipo');
 			}
 			else {
@@ -24,7 +25,33 @@ jQuery(document).ready(function($){
 					
 					console.log(user_info);
 					database.ref('lugar_info').push(user_info);
-        			location.href="resultado.html"
+        			
+			}
+
+			var lugar = $('.formulario #lugar').val();
+			var picnic = $('.formulario #picnic').val();
+			var santa = $('.formulario #santa').val();
+			var marinilla = $('.formulario #marinilla').val();
+			var arvi = $('.formulario #arvi').val();
+			var jardin = $('.formulario #jardin').val();
+			var parque = $('.formulario #parque').val();
+
+			if (lugar == santa) {
+				location.href="resultado.html"
+
+			}else if (lugar == marinilla) {
+				location.href="resultado2.html"
+
+			}else if (lugar == arvi) {
+				location.href="resultado3.html"
+
+			}else if (lugar == jardin) {
+				location.href="resultado4.html"
+
+			}else if (lugar == parque) {
+				location.href="resultado5.html"
+			}else {
+				alert('algo salio mal');
 			}
 		});
 });
