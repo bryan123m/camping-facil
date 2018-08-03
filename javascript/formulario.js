487b1bc27f84c9ff102bd97e7be71596ea9293f8
bryan123m <bryanmzr183@gmail.com>
2018-08-02 20:59:44 -0500

    I achieved it

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -1,47 +1,28 @@
-
-
 jQuery(document).ready(function($){
-
-	$('#tipo').on('change', function(){
-		
-		var lugar=$('.formulario #tipo').val();
-		alert(lugar);
-		if (true) {}
-	})
-
 	$('#buscar').click(
 		'submit',
 		function(e){
 			e.preventDefault();
 
 			var lugar = $('.formulario #lugar').val();
-			console.log(lugar)
-			var cuantos = $('.formulario #cuantos').val();
 			var tipo = $('.formulario #tipo').val();
 			if (tipo == 'Selecciona el tipo') {
 				alert('Debe ingresar un tipo')
-			}else if (lugar == 'Selecciona el lugar') {
-				alert('Debe ingresar un lugar');
-			}else if (!cuantos) {
-				alert('Debe ingresar cuantos son');
 			}else if (lugar == 'Selecciona el lugar') {
 				alert('Debe ingresar un lugar');
 			}
 			else {
 				var user_info = {
 						lugar:$('.formulario #lugar').val(),
-						departamento:$('.formulario #cuantos').val(),
-						ciudad:$('.formulario #tipo').val(),
+						tipo:$('.formulario #tipo').val(),
 
 					};
 					
-					console.log(user_info);
 					database.ref('lugar_info').push(user_info);
         			
 			}
 
 			var lugar = $('.formulario #lugar').val();
-			var picnic = $('.formulario #picnic').val();
 			var santa = $('.formulario #santa').val();
 			var marinilla = $('.formulario #marinilla').val();
 			var arvi = $('.formulario #arvi').val();
